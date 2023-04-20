f22febf54c972001d62ec7c596fe5bc1425a1b49
DESKTOP-TS2N1UG\lg <qnsghd0112@naver.com>
2023-04-20 10:39:46 +0900

    feat: Codeup1011 추가

---
+++ src/main/java/com/example/javaproject2/codeup/Codeup1011.java
@@ -0,0 +1,12 @@
+package com.example.javaproject2.codeup;
+
+import java.util.Scanner;
+
+public class Codeup1011 {
+    public static void main(String[] args) {
+        String x;
+        Scanner sc = new Scanner(System.in);
+        x = sc.next();
+        System.out.println(x);
+    }
+}
