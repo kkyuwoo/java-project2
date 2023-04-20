521502ee49d55da734f392a9c93e92ecc705e688
DESKTOP-TS2N1UG\lg <qnsghd0112@naver.com>
2023-04-20 10:38:49 +0900

    feat: ScannerReview 수정 - 입력받을 때 안내문 추가 - 덧셈 제거 - 출력 안내문 추가

--- src/main/java/com/example/javaproject2/week1/ScannerReview.java
+++ src/main/java/com/example/javaproject2/week1/ScannerReview.java
@@ -5,6 +5,10 @@ import java.util.Scanner;
 public class ScannerReview {
     public void readNumbersAndPlus() {
         Scanner sc = new Scanner(System.in);
-        System.out.println(sc.nextInt() + sc.nextInt());
+        System.out.println("두 개의 숫자를 입력하세요. 입력하고 엔터 입력하고 엔터");
+        System.out.print("첫 번째 숫자 : ");
+        System.out.println("첫 번째 숫자는 " + sc.nextInt() + "입니다.");
+        System.out.print("두 번째 숫자 : ");
+        System.out.println("두 번째 숫자는 " + sc.nextInt() + "입니다.");
     }
 }
