fd5f1be62befc1576392f52f175557c5f1d9bb04
DESKTOP-TS2N1UG\lg <qnsghd0112@naver.com>
2023-04-20 16:20:14 +0900

    feat: Codeup1024 추가

---
+++ src/main/java/com/example/javaproject2/codeup/Codeup1024.java
@@ -0,0 +1,14 @@
+package com.example.javaproject2.codeup;
+
+import java.util.Scanner;
+
+public class Codeup1024 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String word = sc.next();
+        String array[] = word.split("");
+        for(int i=0; i<word.length(); i++) {
+            System.out.println("'" + array[i] + "'");
+        }
+    }
+}
