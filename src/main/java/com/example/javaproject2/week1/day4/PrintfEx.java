c3d668fd03f507d28976f8d865cdab2fe5c1ed39
DESKTOP-TS2N1UG\lg <qnsghd0112@naver.com>
2023-04-20 11:20:31 +0900

    feat: PrintfEx 추가

---
+++ src/main/java/com/example/javaproject2/week1/day4/PrintfEx.java
@@ -0,0 +1,13 @@
+package com.example.javaproject2.week1.day4;
+
+public class PrintfEx {
+    public void print() {
+        System.out.printf("%s", "Hello\n");
+        System.out.printf("bye");
+    }
+
+    public static void main(String[] args) {
+        PrintfEx printfEx = new PrintfEx(); //printfEx 나와라
+        printfEx.print(); //printfEx야 물대포를 쏴줘
+    }
+}
