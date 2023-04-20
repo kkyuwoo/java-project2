8c567d1bbf7a6a677b3fccfc472b66de4875853c
DESKTOP-TS2N1UG\lg <qnsghd0112@naver.com>
2023-04-20 16:11:39 +0900

    feat: Codeup1023 추가

---
+++ src/main/java/com/example/javaproject2/codeup/Codeup1023.java
@@ -0,0 +1,12 @@
+package com.example.javaproject2.codeup;
+
+import java.util.Scanner;
+
+public class Codeup1023 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String float1[] = sc.next().split("[.]");
+        System.out.println(float1[0]);
+        System.out.println(float1[1]);
+    }
+}
