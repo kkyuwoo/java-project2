bb31cda852ff81eeb63d01926d940788b4ac44c4
DESKTOP-TS2N1UG\lg <qnsghd0112@naver.com>
2023-04-20 13:39:28 +0900

    feat: printFloat 추가 - AverageEx 추가 - FloatFormatEx 추가

---
+++ src/main/java/com/example/javaproject2/week1/day4/FloatFormatEx.java
@@ -0,0 +1,9 @@
+package com.example.javaproject2.week1.day4;
+
+public class FloatFormatEx {
+    public static void main(String[] args) {
+        System.out.printf("%f\n", 0.5);
+        System.out.printf("%.1f\n", 3.389);
+        System.out.printf("%.2f\n", 3.389);
+    }
+}
