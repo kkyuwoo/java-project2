d3bb82167a12115407ebcc3e79e6b6e1a01f7277
DESKTOP-TS2N1UG\lg <qnsghd0112@naver.com>
2023-04-20 15:58:36 +0900

    feat: Codeup1020 추가

---
+++ src/main/java/com/example/javaproject2/codeup/Codeup1020.java
@@ -0,0 +1,11 @@
+package com.example.javaproject2.codeup;
+
+import java.util.Scanner;
+
+public class Codeup1020 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String number[] = sc.next().split("-");
+        System.out.println(number[0] + number[1]);
+    }
+}
