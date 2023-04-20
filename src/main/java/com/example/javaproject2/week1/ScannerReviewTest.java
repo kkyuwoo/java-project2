88ba67d35c236633874cb520e14ff4bbc71a5945
DESKTOP-TS2N1UG\lg <qnsghd0112@naver.com>
2023-04-20 09:24:47 +0900

    feat: ScannerReview 추가 - ScannerReview, ScannerReviewTest 추가

---
+++ src/main/java/com/example/javaproject2/week1/ScannerReviewTest.java
@@ -0,0 +1,8 @@
+package com.example.javaproject2.week1;
+
+public class ScannerReviewTest {
+    public static void main(String[] args) {
+        ScannerReview sr = new ScannerReview();
+        sr.readNumbersAndPlus();
+    }
+}
