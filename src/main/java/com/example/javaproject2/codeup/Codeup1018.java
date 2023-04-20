fb201e5193bbe93f17234bea298dba5568ab7649
DESKTOP-TS2N1UG\lg <qnsghd0112@naver.com>
2023-04-20 14:24:42 +0900

    feat: Codeup1018 추가

---
+++ src/main/java/com/example/javaproject2/codeup/Codeup1018.java
@@ -0,0 +1,11 @@
+package com.example.javaproject2.codeup;
+
+import java.util.Scanner;
+
+public class Codeup1018 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String time[] = sc.next().split(":");
+        System.out.println(time[0] + ":" + time[1]);
+    }
+}
