3988f9716d775b0a2e00e3903410048a1da63d1a
DESKTOP-TS2N1UG\lg <qnsghd0112@naver.com>
2023-04-21 15:15:53 +0900

    feat: User 클래스와 멤버 변수로 표현 - User.java, UserTest 추가

---
+++ src/main/java/com/example/javaproject2/week1/day5/UserTest.java
@@ -0,0 +1,15 @@
+package com.example.javaproject2.week1.day5;
+
+public class UserTest {
+    public static void main(String[] args) {
+        User user = new User();
+        user.name = "김규리";
+        user.phoneNumber = "010-1234-5678";
+        user.age = 20;
+
+        User kyeongrok = new User();
+        kyeongrok.name = "김경록";
+        kyeongrok.phoneNumber = "010-0000-1234";
+        kyeongrok.age = 37;
+    }
+}
