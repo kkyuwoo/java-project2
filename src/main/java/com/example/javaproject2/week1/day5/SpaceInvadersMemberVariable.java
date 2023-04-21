ce8fb24d9221fbfd962bf2f9e3bbeb0b70af16c0
DESKTOP-TS2N1UG\lg <qnsghd0112@naver.com>
2023-04-21 14:12:06 +0900

    feat: SpaceInvadersMemberVariable 수정 - SpaceInvader 멤버 변수로 접근

--- src/main/java/com/example/javaproject2/week1/day5/SpaceInvadersMemberVariable.java
+++ src/main/java/com/example/javaproject2/week1/day5/SpaceInvadersMemberVariable.java
@@ -5,12 +5,15 @@ public class SpaceInvadersMemberVariable {
 
     public void moveLeft() {
         location = location - 1;
+    }
+
+    public void moveLeftAndPrint() {
+        location = location - 1;
         System.out.printf("moveLeft: %d\n", location);
     }
 
     public void moveRight() {
         location = location + 1;
-        System.out.printf("moveRight: %d\n", location);
     }
 
     public static void main(String[] args) {
@@ -20,5 +23,9 @@ public class SpaceInvadersMemberVariable {
         simv.moveRight();
         simv.moveRight();
         simv.moveRight();
+
+        System.out.printf("최종 위치: %d\n", simv.location);
+        simv.moveLeft();
+        System.out.printf("최종 위치: %d\n", simv.location);
     }
 }
