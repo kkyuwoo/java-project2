ea10924522954b6eb00d270e723fef47607c11a1
DESKTOP-TS2N1UG\lg <qnsghd0112@naver.com>
2023-04-21 13:19:03 +0900

    feat: SpaceInvader 수정 - 주석 추가

--- src/main/java/com/example/javaproject2/week1/day5/SpaceInvader.java
+++ src/main/java/com/example/javaproject2/week1/day5/SpaceInvader.java
@@ -4,9 +4,17 @@ public class SpaceInvader {
     private void moveLeft() {
         int location = 0;
         location = location - 1;
+        System.out.printf("moveLeft: %d\n", location);
     }
     private void moveright() {
         int location = 0;
         location = location + 1;
+        System.out.printf("moveRight: %d\n", location);
+    }
+
+    public static void main(String[] args) {
+        SpaceInvader si = new SpaceInvader();
+        si.moveLeft(); // 키보드 왼쪽 키 누르면 실행되는 메소드
+        si.moveright(); // 키보드 오른쪽 키 누르면 실행되는 메소드
     }
 }
