1f74294c5c5d14ffb71d58c92c7fcb798591b589
DESKTOP-TS2N1UG\lg <qnsghd0112@naver.com>
2023-04-24 11:34:24 +0900

    feat : FloatLength 추가 - float형은 7자리 초과하면 반올림

---
+++ src/main/java/com/example/javaproject2/week2/FloatLength.java
@@ -0,0 +1,8 @@
+package com.example.javaproject2.week2;
+
+public class FloatLength {
+    public static void main(String[] args) {
+        float f1 = 3.141592653589793f; //7자리 초과하면 반올림
+        System.out.println(f1);
+    }
+}
