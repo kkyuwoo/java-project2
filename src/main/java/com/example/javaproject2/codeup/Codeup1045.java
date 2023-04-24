2695192376fadf611a714739f64ade34ad0b1afd
DESKTOP-TS2N1UG\lg <qnsghd0112@naver.com>
2023-04-24 18:53:56 +0900

    feat : Codeup1045 추가

---
+++ src/main/java/com/example/javaproject2/codeup/Codeup1045.java
@@ -0,0 +1,17 @@
+package com.example.javaproject2.codeup;
+
+import java.util.Scanner;
+
+public class Codeup1045 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+        System.out.printf("%.2f", (float)a/b);
+    }
+}
