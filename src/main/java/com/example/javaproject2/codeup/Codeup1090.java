265469986eed94017831494e27de14f33beb684b
DESKTOP-TS2N1UG\lg <qnsghd0112@naver.com>
2023-04-26 21:10:23 +0900

    feat : Codeup1090 추가

---
+++ src/main/java/com/example/javaproject2/codeup/Codeup1090.java
@@ -0,0 +1,13 @@
+package com.example.javaproject2.codeup;
+
+import java.util.Scanner;
+
+public class Codeup1090 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int r = sc.nextInt();
+        int n = sc.nextInt();
+        System.out.printf("%.0f", a*Math.pow(r, (n-1)));
+    }
+}
