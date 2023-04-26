133db855021eee62c07b6a352f4774e3ad919bbe
DESKTOP-TS2N1UG\lg <qnsghd0112@naver.com>
2023-04-26 10:08:02 +0900

    feat : Teacher, ClassInSchool, ClassInSchoolTest 추가 - Teacher : 선생님 객체 - ClassInSchool : 반 객체 - ClassInSchoolTest : 각 객체 멤버 변수 접근

---
+++ src/main/java/com/example/javaproject2/week2/day3/ClassInSchool.java
@@ -0,0 +1,8 @@
+package com.example.javaproject2.week2.day3;
+
+public class ClassInSchool {
+    int no; //멤버변수
+    //String teacherName; //멤버변수
+    Teacher teacher;
+    Student[] students; //멤버변수
+}
