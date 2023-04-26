d55c048817118540d517df8b2dce971be837fea3
DESKTOP-TS2N1UG\lg <qnsghd0112@naver.com>
2023-04-26 15:29:05 +0900

    feat : Codeup1081 추가

---
+++ src/main/java/com/example/javaproject2/codeup/Codeup1081.java
@@ -0,0 +1,16 @@
+package com.example.javaproject2.codeup;
+
+import java.util.Scanner;
+
+public class Codeup1081 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        for(int i = 1; i<=n; i++) {
+            for(int j = 1; j<=m; j++) {
+                System.out.println(i + " " + j);
+            }
+        }
+    }
+}
