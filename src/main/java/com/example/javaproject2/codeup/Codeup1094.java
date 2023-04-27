f068eb5d0adba8e4188a6fc0fbd18a2a923ecc45
DESKTOP-TS2N1UG\lg <qnsghd0112@naver.com>
2023-04-27 11:30:22 +0900

    feat : Codeup1094 추가

---
+++ src/main/java/com/example/javaproject2/codeup/Codeup1094.java
@@ -0,0 +1,18 @@
+package com.example.javaproject2.codeup;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Codeup1094 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int[] arr = new int[num];
+        for(int i=0; i<num; i++) {
+            arr[i] = sc.nextInt();
+        }
+        for(int i=(num-1); i>=0; i--) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
