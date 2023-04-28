9af15326c3059168afce9ab4a61c066b0574b381
DESKTOP-TS2N1UG\lg <qnsghd0112@naver.com>
2023-04-28 14:21:02 +0900

    feat : Codeup1116~Codeup1121 추가

---
+++ src/main/java/com/example/javaproject2/codeup/Codeup1116.java
@@ -0,0 +1,15 @@
+package com.example.javaproject2.codeup;
+
+import java.util.Scanner;
+
+public class Codeup1116 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.printf("%d+%d=%d\n", a, b, a+b);
+        System.out.printf("%d-%d=%d\n", a, b, a-b);
+        System.out.printf("%d*%d=%d\n", a, b, a*b);
+        System.out.printf("%d/%d=%d\n", a, b, a/b);
+    }
+}
---
+++ src/main/java/com/example/javaproject2/codeup/Codeup1118.java
@@ -0,0 +1,12 @@
+package com.example.javaproject2.codeup;
+
+import java.util.Scanner;
+
+public class Codeup1118 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.printf("%.1f", a*b/2.0);
+    }
+}
