c394e3a49cc2a9a51307815e46f8aaf670708d0c
kkyuwoo <ggya5@naver.com>
2023-05-03 11:41:46 +0900

    feat : DoWhileEx, EvenNumberInput 추가 - Do While 예제

---
+++ src/main/java/com/example/javaproject2/week3/day3/DoWhileEx.java
@@ -0,0 +1,12 @@
+package com.example.javaproject2.week3.day3;
+
+import java.util.Scanner;
+
+public class DoWhileEx {
+    public static void main(String[] args) {
+        int cnt = 10;
+        do {
+            System.out.println("do 구간에서 실행");
+        } while(cnt < 10);
+    }
+}
