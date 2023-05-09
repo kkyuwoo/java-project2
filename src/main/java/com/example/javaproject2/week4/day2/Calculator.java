7a90f6db3ce3570946ddb426f9fbe316aa7a1b5a
kkyuwoo <ggya5@naver.com>
2023-05-09 11:25:02 +0900

    feat : Calculator, CalculatorTest 추가 - Static 메소드 생성, 호출

---
+++ src/main/java/com/example/javaproject2/week4/day2/Calculator.java
@@ -0,0 +1,7 @@
+package com.example.javaproject2.week4.day2;
+
+public class Calculator {
+    public static void printMinus(int num1, int num2) {
+        System.out.println(num1 - num2);
+    }
+}
