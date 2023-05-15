11eb4db1c4ec81f85f46b85dbb848dce5ce395c3
kkyuwoo <ggya5@naver.com>
2023-05-15 15:51:10 +0900

    feat : 버블정렬

---
+++ src/main/java/com/example/javaproject2/week5/day1/BubbleSort03.java
@@ -0,0 +1,36 @@
+package com.example.javaproject2.week5.day1;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class BubbleSort03 {
+    Comparator<Integer> comparator;
+
+    public BubbleSort03(Comparator<Integer> comparator) {
+        this.comparator = comparator;
+    }
+
+    public int[] sortARound(int [] arr, int until) {
+        for (int i = 0; i < until; i++) {
+            if(comparator.compare(arr[i], arr[i+1]) > 0) {
+                int temp = arr[i];
+                arr[i] = arr[i+1];
+                arr[i+1] = temp;
+            }
+        }
+        return arr;
+    }
+    public int[] sort(int[] arr){
+        for (int i = 1; i <= arr.length - 1 ; i++) {
+            arr = sortARound(arr, arr.length - i);
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        BubbleSort03 bs = new BubbleSort03(((o1, o2) -> o2 - o1));
+        int[] arr = {7, 2, 3, 9, 28, 11};
+        arr = bs.sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+}
